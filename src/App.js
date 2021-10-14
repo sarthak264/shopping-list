94274db700d5bb7b356e3980b76b3e0823ce27ca
sarthak264 <sarthaksharm372@gmail.com>
2021-10-14 16:54:02 +0530

    added log in page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ShoppingList from "./components/ShoppingList";
+import logIn from './components/LogIn'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 const App = () => {
   return (
     <>
-      <ShoppingList />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={logIn} />
+        <Route path="/list" component={ShoppingList} />
+      </Switch>
+    </Router>
     </>
   );
 };
---
+++ src/components/LogIn.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./login.css";
+
+const logIn = () => {
+  return (
+    <section className="log_in_page">
+      <button class="log_in_btn">Log in with Google</button>
+    </section>
+  );
+};
+
+export default logIn;
